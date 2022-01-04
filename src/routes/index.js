583530297086b7f68dce65c50219f8073f7ac82c
Suven-p <pandeysuven@gmail.com>
2022-01-04 08:23:59 +0545

    Add connection

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var apiRoute = require('./api/api');
+
+router.use('/api', apiRoute);
 
 router.get('/', (req, res, next) => {
   res.json({ message: 'This is the home page!', success: true });
