5df0fd01af73bb2604fffa6dca2bc038aa7413a6
maskudo <manujkarki101@gmail.com>
2022-01-09 21:26:59 +0545

    initial front-end commit

---
+++ src/online_job_portal_frontend/src/App.js
@@ -0,0 +1,12 @@
+import DummyComponent from "./Components/DummyComponent"
+
+function App() {
+  return (
+    <div className="App">
+      <header><h1>Online Job Portal</h1></header>
+      <DummyComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/online_job_portal_frontend/src/Components/DummyComponent.js
@@ -0,0 +1,8 @@
+function DummyComponent() {
+    return (
+        <div>
+            <p>Dummy Component here.</p>
+        </div>
+    )
+}
+export default DummyComponent
