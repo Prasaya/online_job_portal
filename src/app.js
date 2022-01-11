5533d30eb91c66749d776e222e27be34418fae74
Suven-p <pandeysuven@gmail.com>
2022-01-11 12:39:16 +0545

    Use /src/public instead of /src/client/build for react

--- src/app.js
+++ src/app.js
@@ -6,14 +6,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));
 
 app.use('/api', require('./routes/api/api'));
 
 app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 module.exports = app;
