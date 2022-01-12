c7c4d4f49f996a7da7e90f11b671b738459d6660
maskudo <manujkarki101@gmail.com>
2022-01-12 13:28:29 +0545

    initial cleanup

--- src/online_job_portal_frontend/src/App.js
+++ src/online_job_portal_frontend/src/App.js
@@ -1,10 +1,7 @@
-import DummyComponent from "./Components/DummyComponent"
 
 function App() {
   return (
     <div className="App">
-      <header><h1>Online Job Portal</h1></header>
-      <DummyComponent />
     </div>
   );
 }
--- src/online_job_portal_frontend/src/Components/DummyComponent.js
+++
@@ -1,8 +0,0 @@
-function DummyComponent() {
-    return (
-        <div>
-            <p>Dummy Component here.</p>
-        </div>
-    )
-}
-export default DummyComponent
