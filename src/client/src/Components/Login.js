bb7c1ccd1abc39ce64024e4aa082ad162f445481
maskudo <manujkarki101@gmail.com>
2022-01-17 14:19:55 +0545

    add states to form inputs

--- src/client/src/Components/Login.js
+++ src/client/src/Components/Login.js
@@ -1,13 +1,39 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 function Login(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        console.log(email, password)
+        setEmail('')
+        setPassword('')
+    }
+
     return (
         <div className="container log-in">
             <div className="row justify-content-center my-5">
                 <div className="col-lg-4">
-                    <form action="#">
-                        <input type="email" className="form-control form-control-lg" placeholder="Email" required/>
-                        <input type="password" className="form-control form-control-lg my-2" placeholder="Password" required/>
+                    <form action="#" onSubmit={onSubmit}>
+                        <input type="email" 
+                        id="login-email"
+                        className="form-control form-control-lg" 
+                        placeholder="Email" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)} 
+                        required/>
+
+                        <input type="password" 
+                        id="login-password"
+                        className="form-control form-control-lg my-2" 
+                        placeholder="Password" 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        minLength={7}
+                        maxLength={20}
+                        required/>
                         <div className="d-grid gap-2">
                             <button className="btn btn-primary btn-lg" type="submit">Log In</button>
                         </div>
--- src/client/src/Components/RegisterJobSeeker.js
+++ src/client/src/Components/RegisterJobSeeker.js
@@ -1,21 +1,43 @@
+import {useState} from "react"
+
 function RegisterJobSeeker(){
+    const [firstname, setFirstname] = useState('')
+    const [lastname, setLastname] = useState('')
+    const [email, setEmail] = useState('')
+    const [phonenumber, setPhonenumber] = useState('')
+    const [birthday, setBirthday] = useState('')
+    const [course, setCourse] = useState('bct')
+    const [password, setPassword] = useState('')
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        setFirstname('')
+        setLastname('')
+        setEmail('')
+        setPhonenumber('')
+        setBirthday('')
+        setCourse('bct')
+        setPassword('')
+    }
+
     return (
         <div className="container register-account">
             <div className="row justify-content-center my-5 center-text">
                 <header className="text-center"><h3>Job Seeker Account Registration</h3></header>
                 <div className="col-lg-4">
-                    <form action="#">
-                        <input type="text" className="form-control form-control-lg" placeholder="Firstname" required/>
-                        <input type="text" className="form-control form-control-lg my-2" placeholder="Lastname" required/>
-                        <input type="email" className="form-control form-control-lg my-2" placeholder="Email" required/>
-                        <input type="text" className="form-control form-control-lg my-2" placeholder="Phone Number"/>
-                        <input type="date" className="form-control form-control-lg my-2" placeholder="Birthday" required/>
-                        <select className="form-select form-select-lg my-2">
-                            <option selected>BCT</option>
+                    <form action="#" onSubmit={onSubmit}>
+                        <input type="text" className="form-control form-control-lg" placeholder="Firstname" value={firstname} onChange={(e) => setFirstname(e.target.value)} required/>
+                        <input type="text" className="form-control form-control-lg my-2" placeholder="Lastname" value={lastname} onChange={(e) => setLastname(e.target.value)} required/>
+                        <input type="email" className="form-control form-control-lg my-2" placeholder="Email" value={email} onChange={(e) =>setEmail(e.target.value)} required/>
+                        <input type="text" className="form-control form-control-lg my-2" placeholder="Phone Number" value={phonenumber} onChange={(e) =>setPhonenumber(e.target.value)} />
+                        <input type="date" className="form-control form-control-lg my-2" placeholder="Birthday" value={birthday} onChange={(e) =>setBirthday(e.target.value)} required/>
+                        <select className="form-select form-select-lg my-2" value={course} onChange={(e) =>setCourse(e.target.value)}>
+                            <option value="bct">BCT</option>
                             <option value="bme">BME</option>
                             <option value="bce">BCE</option>
                             <option value="bei">BEI</option>
                         </select>
+                        <input type="password" className="form-control form-control-lg my-2" placeholder="Password" minLength={7} value={password} onChange={(e) =>setPassword(e.target.value)} required></input>
                         <div className="d-grid gap-2">
                             <button className="btn btn-success btn-lg" type="submit">Sign Up</button>
                         </div>
