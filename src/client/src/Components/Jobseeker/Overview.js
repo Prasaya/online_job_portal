d387c43bf076dfddfa70c4be08121cad4f2df6cd
maskudo <manujkarki101@gmail.com>
2022-01-18 23:06:41 +0545

    initial overview page commit

---
+++ src/client/src/Components/Jobseeker/Overview.js
@@ -0,0 +1,13 @@
+function Overview() {
+    // const Onclick = () => {
+    //     const currenActive = document.querySelector('.active')
+    // }
+
+    return ( 
+        <div className="overview">
+            <h1>Overview</h1>
+        </div>
+    )
+}
+
+export default Overview
