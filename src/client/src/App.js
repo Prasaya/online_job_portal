ac15003d9f8487c1e61010dbe35709298fe5ddd4
maskudo <manujkarki101@gmail.com>
2022-01-18 23:03:32 +0545

    add routing for jobseeker homepage

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -2,16 +2,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import Login from "./Components/Login";
 import RegisterJobSeeker from "./Components/RegisterJobSeeker";
+import JobseekerHome from "./Components/Jobseeker/Home"
+import Overview from "./Components/Jobseeker/Overview";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App container">
+      <div className="App">
         <Header />
         <Routes>
           <Route path="/" element={<Login />} exact />
           <Route path="/login" element={<Login />} />
           <Route path="/register-jobseeker" element={<RegisterJobSeeker />} />
+          <Route path="/jobseeker" element={<JobseekerHome />} > 
+            <Route path="overview" element={<Overview/>} />
+          </Route> 
         </Routes>
       </div>
     </BrowserRouter>
