08779322ed040d42d0a6c079db668b668383528e
maskudo <manujkarki101@gmail.com>
2022-01-19 17:05:57 +0545

    display jobs fetched from json server

--- src/client/src/Components/Jobseeker/Overview.js
+++ src/client/src/Components/Jobseeker/Overview.js
@@ -1,7 +1,40 @@
+import {useState, useEffect} from "react"
+
 function Overview() {
+    const [jobs, setJobs] = useState([])
+
+    async function fetchJobs(){
+        const res = await fetch("http://localhost:4000/jobs")
+        const data = await res.json()
+        return data
+    }
+
+    useEffect(() => {
+        async function getJobs(){
+            const overviewJobs = await fetchJobs()
+            setJobs(overviewJobs) 
+        }
+        getJobs()
+    })
+
     return ( 
         <div className="overview">
             <h1>Overview</h1>
+            <div className="jobs container bg-light">
+                {jobs.map((job) => {
+                    return (
+                        <div className="job d-flex flex-row justify-content-between align-items-center my-2" id={job.id}>
+                            <div className="d-flex flex-column">
+                                <h3>{job.title}</h3>
+                                <h6>Deadline: {job.deadline}</h6>
+                            </div>
+                            <div>
+                                <button className="btn btn-primary">{job.applied?"Unapply":"Apply"}</button>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
