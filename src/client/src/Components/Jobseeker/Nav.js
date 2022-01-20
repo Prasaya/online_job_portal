129f332b92043564e1fd48a3df4809f11b3b2575
maskudo <manujkarki101@gmail.com>
2022-01-20 22:03:41 +0545

    refactor and add highlight to active nav link

--- src/client/src/Components/Jobseeker/Nav.js
+++ src/client/src/Components/Jobseeker/Nav.js
@@ -1,24 +1,56 @@
 import { Link } from "react-router-dom"
-//todo:highlight active tab
+import {useState} from "react"
+
 function Nav(){
+    const [navElements, setNavElements] = useState([
+        {
+            name: "Overview",
+            to:"overview",
+            status: "active" 
+        },
+        {
+            name: "My Status",
+            to:"mystatus",
+            status: ""
+        },
+        {
+            name: "My Profile",
+            to:"myprofile",
+            status: ""
+        },
+        {
+            name: "Settings",
+            to: "settings",
+            status: ""
+        }]
+    )
+    function OnClick(e){
+        const id = e.target.id
+        const updatedNavElements = navElements.map((element) => {
+            if (element.status === "active"){
+                element.status = ""
+            }
+            if (element.to === id){
+                element.status = "active"
+            }
+            return element
+        })
+        setNavElements(updatedNavElements)
+    }
+
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light py-2">
             <div className="container-fluid d-flex flex-row justify-content-between">
                 <Link className="navbar-brand left" to="/">Job Portal</Link>
                 <div className="right">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="overview">Overview</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="mystatus">My Status</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="myprofile">My Profile</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="settings">Settings</Link>
-                            </li>
+                            {navElements.map((element) => {
+                                return(
+                                    <li className="nav-link">
+                                        <Link onClick={OnClick} className={`nav-link ${element.status}`} to={element.to} id={`${element.to}`}>{element.name}</Link>
+                                    </li>
+                                )
+                            })}
                         </ul>
                 </div>
             </div>
