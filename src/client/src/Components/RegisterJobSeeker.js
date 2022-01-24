2202b0fc9f126c372f1ac8f0434a08dfa6464556
maskudo <manujkarki101@gmail.com>
2022-01-24 23:05:53 +0545

    refactor form using react-hook-form

--- src/client/src/Components/RegisterJobSeeker.js
+++ src/client/src/Components/RegisterJobSeeker.js
@@ -1,43 +1,27 @@
-import {useState} from "react"
+import {useForm} from "react-hook-form"
 
 function RegisterJobSeeker(){
-    const [firstname, setFirstname] = useState('')
-    const [lastname, setLastname] = useState('')
-    const [email, setEmail] = useState('')
-    const [phonenumber, setPhonenumber] = useState('')
-    const [birthday, setBirthday] = useState('')
-    const [faculty, setFaculty] = useState('bct')
-    const [password, setPassword] = useState('')
-
-    const onSubmit = (e) => {
-        e.preventDefault()
-        setFirstname('')
-        setLastname('')
-        setEmail('')
-        setPhonenumber('')
-        setBirthday('')
-        setFaculty('bct')
-        setPassword('')
-    }
+    const {register, handleSubmit} = useForm()
+    const onSubmitForm = (data) => console.log(data)
 
     return (
         <div className="container register-account">
             <div className="row justify-content-center my-5 center-text">
                 <header className="text-center"><h3>Job Seeker Account Registration</h3></header>
                 <div className="col-lg-4">
-                    <form action="#" onSubmit={onSubmit}>
-                        <input type="text" name="firstname" className="form-control form-control-lg" placeholder="Firstname" value={firstname} onChange={(e) => setFirstname(e.target.value)} required/>
-                        <input type="text" name="lastname" className="form-control form-control-lg my-2" placeholder="Lastname" value={lastname} onChange={(e) => setLastname(e.target.value)} required/>
-                        <input type="email" name="email" className="form-control form-control-lg my-2" placeholder="Email" value={email} onChange={(e) =>setEmail(e.target.value)} required/>
-                        <input type="text" name="phonenumber" className="form-control form-control-lg my-2" placeholder="Phone Number" value={phonenumber} onChange={(e) =>setPhonenumber(e.target.value)} />
-                        <input type="date" name="birthday" className="form-control form-control-lg my-2" placeholder="Birthday" value={birthday} onChange={(e) =>setBirthday(e.target.value)} required/>
-                        <select name="faculty" className="form-select form-select-lg my-2" value={faculty} onChange={(e) =>setFaculty(e.target.value)}>
+                    <form action="#" onSubmit={handleSubmit(onSubmitForm)}>
+                        <input {...register("firstname", {required:true})} type="text" className="form-control form-control-lg" placeholder="Firstname" />
+                        <input {...register("lastname", {required:true})} type="text" className="form-control form-control-lg my-2" placeholder="Lastname" />
+                        <input {...register("email", {required:true})} type="email"  className="form-control form-control-lg my-2" placeholder="Email" />
+                        <input {...register("phone",{})} type="text" defaultValue={""} className="form-control form-control-lg my-2" placeholder="Phone Number"  />
+                        <input {...register("birthday", {required:true})} type="date" className="form-control form-control-lg my-2" placeholder="Birthday" />
+                        <select {...register("faculty", {required:true})} className="form-select form-select-lg my-2" >
                             <option value="bct">BCT</option>
                             <option value="bme">BME</option>
                             <option value="bce">BCE</option>
                             <option value="bei">BEI</option>
                         </select>
-                        <input name="password" type="password" className="form-control form-control-lg my-2" placeholder="Password" minLength={7} value={password} onChange={(e) =>setPassword(e.target.value)} required></input>
+                        <input {...register("password", {required:true, pattern:/(?=.*)(?=.*[a-z])(?=.*[A-Z]).{8,}/})} type="password" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" className="form-control form-control-lg my-2" placeholder="Password" ></input>
                         <div className="d-grid gap-2">
                             <button className="btn btn-success btn-lg" type="submit">Sign Up</button>
                         </div>
