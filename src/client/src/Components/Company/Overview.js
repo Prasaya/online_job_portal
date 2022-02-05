dab517795851ad93cc9e9c373c79013b56f7d03c
Suprim Shrestha <suprim.shresth@gmail.com>
2022-02-05 22:02:16 +0545

    Add button to New Job Post

--- src/client/src/Components/Company/Overview.js
+++ src/client/src/Components/Company/Overview.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 function Overview() {
   return (
     <div>
       <h1>Posted Jobs</h1>
+      <Link to="/company/jobpost" className="btn btn-primary btn-lg my-5">Add Job</Link>
     </div>
   );
 }
