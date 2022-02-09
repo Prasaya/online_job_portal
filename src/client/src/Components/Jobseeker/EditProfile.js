f96061e5dc92367e80a40d7937022c982019b3ea
maskudo <manujkarki101@gmail.com>
2022-02-09 16:07:13 +0545

    create vertical tabs

--- src/client/src/Components/Jobseeker/EditProfile.js
+++ src/client/src/Components/Jobseeker/EditProfile.js
@@ -1,60 +1,111 @@
 import { useForm } from "react-hook-form"
 import {useState, useEffect} from "react"
+import {Link, useLocation, Outlet} from "react-router-dom"
 
 
 function MyProfile() {
-    const {register, handleSubmit} = useForm()
-    const onSubmitForm = (data) => {
-
-        data.basics.firstname? console.log() :data.basics.firstname = basicInfo.firstname
-        data.basics.lastname? console.log() :data.basics.lastname = basicInfo.lastname
-        data.basics.email? console.log() :data.basics.email = basicInfo.email
-        data.basics.phone? console.log() :data.basics.phone = basicInfo.phone
-        data.basics.birthday? console.log() :data.basics.birthday = basicInfo.birthday
-        data.basics.faculty? console.log() :data.basics.faculty = basicInfo.faculty
-
-
-        data.socials.facebook? console.log() :data.socials.facebook = socials.facebook
-        data.socials.github? console.log() :data.socials.github = socials.github
-        data.socials.twitter? console.log() :data.socials.twitter = socials.twitter
-        data.skills? console.log() :data.skills = skills
-
-        fetch(`/api/user/uid`,{
-                method: "PUT",
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            })
-
-        console.log(data)
+    const path = useLocation().pathname
+    const [navElements, setNavElements] = useState([
+        {
+            name: "Avatar",
+            to:"avatar",
+            status: "" 
+        },
+        {
+            name: "Basic Info",
+            to:"basics",
+            status: ""
+        },
+        {
+            name: "Education",
+            to:"education",
+            status: ""
+        },
+        {
+            name: "Skills",
+            to: "skills",
+            status: ""
+        }]
+    )
+    function OnClick(e){
+        const id = e.target.id
+        const updatedNavElements = navElements.map((element) => {
+            if (element.status === "table-active"){
+                element.status = ""
+            }
+            if (element.to === id){
+                element.status = "table-active"
+            }
+            return element
+        })
+        setNavElements(updatedNavElements)
     }
-    
-    const [basicInfo, setBasicInfo] = useState('')
-    const [skills, setSkills] = useState('')
-    const [socials, setSocials] = useState('')
-
-
-    const fetchInfo = async () => {
-        const res = await fetch("/api/user/uid")
-        const data = await res.json()
-        return data
-    }
-    
+    //set active for initial render
     useEffect(() => {
-        const getInfo = async () => {
-            const info = await fetchInfo()
-            setBasicInfo(info["basics"])
-            setSkills(info["skills"])
-            setSocials(info["socials"])            
-        }
-        getInfo()        
-    }, [])
+        const updatedNavElements = navElements.map((element) => {
+            if (element.status === "table-active"){
+                element.status = ""
+            }
+            if (path.includes(element.to) ){
+                element.status = "table-active"
+            }
+            return element
+        })
+        setNavElements(updatedNavElements)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    // const {register, handleSubmit} = useForm()
+    // const onSubmitForm = (data) => {
+
+    //     data.basics.firstname? console.log() :data.basics.firstname = basicInfo.firstname
+    //     data.basics.lastname? console.log() :data.basics.lastname = basicInfo.lastname
+    //     data.basics.email? console.log() :data.basics.email = basicInfo.email
+    //     data.basics.phone? console.log() :data.basics.phone = basicInfo.phone
+    //     data.basics.birthday? console.log() :data.basics.birthday = basicInfo.birthday
+    //     data.basics.faculty? console.log() :data.basics.faculty = basicInfo.faculty
+
+
+    //     data.socials.facebook? console.log() :data.socials.facebook = socials.facebook
+    //     data.socials.github? console.log() :data.socials.github = socials.github
+    //     data.socials.twitter? console.log() :data.socials.twitter = socials.twitter
+    //     data.skills? console.log() :data.skills = skills
+
+    //     fetch(`/api/user/uid`,{
+    //             method: "PUT",
+    //             headers: {
+    //                 'Content-type': 'application/json'
+    //             },
+    //             body: JSON.stringify(data)
+    //         })
+
+    //     console.log(data)
+    // }
+    
+    // const [basicInfo, setBasicInfo] = useState('')
+    // const [skills, setSkills] = useState('')
+    // const [socials, setSocials] = useState('')
+
+
+    // const fetchInfo = async () => {
+    //     const res = await fetch("http://localhost:4000/profile")
+    //     const data = await res.json()
+    //     return data
+    // }
+    
+    // useEffect(() => {
+    //     const getInfo = async () => {
+    //         const info = await fetchInfo()
+    //         setBasicInfo(info["basics"])
+    //         setSkills(info["skills"])
+    //         setSocials(info["socials"])            
+    //     }
+    //     getInfo()        
+    // }, [])
     return (
         <div className="editprofile">
             <h1>Edit Profile</h1>
             <div className="bg-light container">
-                <div className="d-flex justify-content-center">
+                {/* <div className="d-flex justify-content-center">
                     <form className="col-lg-4" action="#" onSubmit={handleSubmit(onSubmitForm)}>
                         <fieldset>
                             <legend>Basic Information</legend>
@@ -102,6 +153,24 @@ function MyProfile() {
                         </fieldset>
                         <button className="btn btn-primary" type="submit">Save</button>
                     </form>
+                </div> */}
+                <div className="row">
+                    <div className="col-12 col-lg-3">
+                        <table className="table table-hover">
+                            <tbody>
+                                {navElements.map((element) => {
+                                    return(
+                                        <tr className="">
+                                            <Link onClick={OnClick} className={`nav-link ${element.status}`} to={element.to} id={`${element.to}`}>{element.name}</Link>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="col-12 col-lg-9">
+                        <h1>Lol</h1>
+                    </div>
                 </div>
             </div>
         </div>
