67d7f4d15fb99c4a5a539e210c0061cbf1582975
Suven Pandey <pandeysuven@gmail.com>
2022-02-11 20:08:36 +0545

    Add and update procedures

--- config/dbProcedures.sql
+++ config/dbProcedures.sql
@@ -1,8 +1,7 @@
 DELIMITER |
-
 DROP PROCEDURE IF EXISTS getAuthDetails |
 CREATE PROCEDURE getAuthDetails(
-    IN inputEmail VARCHAR(250)
+    IN inputEmail VARCHAR(255)
 )
 BEGIN
     IF EXISTS(
@@ -10,19 +9,23 @@ BEGIN
         WHERE users.email = inputEmail
     ) THEN
     BEGIN
-        SELECT u.uid as id, email, password, 'User' AS type
+        SELECT u.uid as id, email, password, roleName as role,
+            JSON_ARRAYAGG(json_object('provider', providerName, 'identifier', identifier)) AS socials
         FROM users AS u
+        INNER JOIN roles AS r on r.roleId = u.role
         LEFT JOIN federated_credentials AS fc ON fc.uid = u.uid
         LEFT JOIN federated_credentials_provider AS fcp on fcp.providerId = fc.providerID
-        WHERE email = inputEmail;
+        WHERE email = inputEmail
+        GROUP BY u.uid;
     END;
     ELSEIF EXISTS(
-        SELECT cid FROM companies
-        WHERE companies.email = inputEmail
+        SELECT oid FROM organizations
+        WHERE organizations.email = inputEmail
     ) THEN
     BEGIN
-        SELECT cid as id, email, password, 'Company' AS type
-        FROM companies AS c
+        SELECT oid as id, email, password, roleName as role
+        FROM organizations AS o
+        INNER JOIN roles AS r on r.roleId = o.role
         WHERE email = inputEmail;
     END;
 
@@ -37,21 +40,67 @@ CREATE PROCEDURE checkExistingUser(
 )
 BEGIN
     DECLARE id VARCHAR(36);
-    DECLARE type VARCHAR(10);
-    SELECT uid into id FROM users WHERE users.email = inputEmail;
+    DECLARE idType VARCHAR(20);
+    SELECT uid, roleName into id, idType
+    FROM users AS u
+    INNER JOIN roles AS r on r.roleId = u.role
+    WHERE email = inputEmail;
     IF isnull(id) THEN
     BEGIN
-		SELECT cid into id FROM companies WHERE companies.email = inputEmail;
-        IF NOT isnull(id) THEN
-			SET type = 'Company';
-		END IF;
-	END;
-    ELSE
-	BEGIN
-		SET type = 'User';
+		SELECT oid, roleName into id, idType
+        FROM organizations AS o
+        INNER JOIN roles AS r on r.roleId = o.role
+        WHERE email = inputEmail;
 	END;
     END IF;
-    SELECT id, type;
-
+    SELECT id, idType;
 END |
 DELIMITER ;
+
+DELIMITER |
+DROP PROCEDURE IF EXISTS getUserData |
+CREATE PROCEDURE getUserData(
+    IN uid CHAR(36)
+)
+BEGIN
+    SELECT u.uid AS id, roleName AS role, email, NULL as password, firstName, middleName, lastName,
+        picture, birthday, phone, gender,
+        JSON_ARRAYAGG(json_object('provider', providerName, 'identifier', identifier)) AS socials
+    FROM users AS u
+    INNER JOIN roles AS r ON r.roleId = u.role
+    LEFT JOIN federated_credentials AS fc ON fc.uid = u.uid
+    LEFT JOIN federated_credentials_provider AS fcp ON fcp.providerID = fc.providerID
+    WHERE u.uid = uid
+    GROUP BY u.uid;
+END |
+DELIMITER ;
+
+DELIMITER |
+DROP PROCEDURE IF EXISTS getOrganizationData |
+CREATE PROCEDURE getOrganizationData(
+    IN oid CHAR(36)
+)
+BEGIN
+    SELECT o.oid as id, roleName AS role, email, NULL as password, name, description, address, city,
+        website, phone, logo
+    FROM organizations AS o
+    INNER JOIN roles AS r ON r.roleId = o.role
+    WHERE o.oid = oid;
+END |
+DELIMITER ;
+
+DELIMITER |
+DROP PROCEDURE IF EXISTS getQualifications;
+CREATE PROCEDURE getQualifications()
+BEGIN
+	SELECT level, json_arrayagg(disc_deg) AS data FROM
+	(
+		SELECT
+			level,
+			json_object(discipline, json_arrayagg(json_object('id', qid, 'name', degree))) AS disc_deg
+		FROM academic_qualifications GROUP BY level, discipline
+	) AS disc
+	GROUP BY level;
+END |
+DELIMITER ;
+
