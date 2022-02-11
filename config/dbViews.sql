a9ee4621d5ff53b7a0e8943e68f2ef686d0a9096
Suven Pandey <pandeysuven@gmail.com>
2022-02-11 20:08:55 +0545

    Update views

--- config/dbViews.sql
+++ config/dbViews.sql
@@ -1,10 +1,15 @@
+DROP VIEW IF EXISTS vwUser;
+DROP VIEW IF EXISTS vwOrganization;
+
 CREATE OR REPLACE VIEW vwUser AS
-	SELECT u.uid, email, firstName, middleName, lastName, picture, birthday, phone, gender,
-        providerName, identifier, 'User' AS type
+	SELECT u.uid, roleName AS role, email, NULL as password, firstName, middleName, lastName, picture, birthday, phone, gender,
+        providerName, identifier
     FROM users AS u
+    INNER JOIN roles AS r ON r.roleId = u.role
     LEFT JOIN federated_credentials AS fc ON fc.uid = u.uid
     LEFT JOIN federated_credentials_provider AS fcp ON fcp.providerID = fc.providerID;
 
-CREATE OR REPLACE VIEW vwCompany AS
-    SELECT cid, name, description, address, city, email, website, phone, logo, 'Company' as type
-    FROM companies;
+CREATE OR REPLACE VIEW vwOrganization AS
+    SELECT oid, roleName as role, email, password, name, description, address, city, website, phone, logo 
+    FROM organizations AS c
+    INNER JOIN roles AS r on r.roleId = c.role;
