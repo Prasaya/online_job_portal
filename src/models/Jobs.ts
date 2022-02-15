1c4bd3a39e706d73e3c66edb6de1e9e7b6e58a46
Prasaya <prasaya.acharya@gmail.com>
2022-02-15 18:53:52 +0545

    Used jobDelete procedure

--- src/models/Jobs.ts
+++ src/models/Jobs.ts
@@ -6,151 +6,146 @@ import { FieldPacket, RowDataPacket } from 'mysql2';
 import logger from '@utils/logger';
 
 export const JobCreationSchema: Schema = {
-  companyId: {
-    in: ['body'],
-    isString: true,
-    isLength: { options: [{ max: 36 }] },
-  },
-  title: {
-    in: ['body'],
-    isString: true,
-    isLength: { options: [{ min: 1, max: 100 }] },
-  },
-  description: {
-    in: ['body'],
-    optional: true,
-    isString: true,
-    isLength: { options: [{ max: 1000 }] },
-  },
-  vacancies: {
-    in: ['body'],
-    optional: true,
-    isNumeric: true,
-  },
-  address: {
-    in: ['body'],
-    optional: true,
-    isString: true,
-    isLength: { options: [{ max: 1000 }] },
-  },
-  district: {
-    in: ['body'],
-    optional: true,
-    isString: true,
-    isLength: { options: [{ max: 1000 }] },
-  },
-  qualifications: {
-    in: ['body'],
-    isArray: true,
-  },
-  'qualifications.*': { isNumeric: true },
-  skills: {
-    in: ['body'],
-    isArray: true,
-  },
-  'skills.*.skillName': {
-    isString: true,
-    isLength: { options: [{ min: 1, max: 100 }] },
-  },
-  'skills.*.proficiency': {
-    isString: true,
-    isIn: {
-      options: [['Beginner', 'Intermediate', 'Advanced']],
-      errorMessage: 'Invalid Skills Proficiency',
+    companyId: {
+        in: ['body'],
+        isString: true,
+        isLength: { options: [{ max: 36 }] },
     },
-  },
+    title: {
+        in: ['body'],
+        isString: true,
+        isLength: { options: [{ min: 1, max: 100 }] },
+    },
+    description: {
+        in: ['body'],
+        optional: true,
+        isString: true,
+        isLength: { options: [{ max: 1000 }] },
+    },
+    vacancies: {
+        in: ['body'],
+        optional: true,
+        isNumeric: true,
+    },
+    address: {
+        in: ['body'],
+        optional: true,
+        isString: true,
+        isLength: { options: [{ max: 1000 }] },
+    },
+    district: {
+        in: ['body'],
+        optional: true,
+        isString: true,
+        isLength: { options: [{ max: 1000 }] },
+    },
+    qualifications: {
+        in: ['body'],
+        isArray: true,
+    },
+    'qualifications.*': { isNumeric: true },
+    skills: {
+        in: ['body'],
+        isArray: true,
+    },
+    'skills.*.skillName': {
+        isString: true,
+        isLength: { options: [{ min: 1, max: 100 }] },
+    },
+    'skills.*.proficiency': {
+        isString: true,
+        isIn: {
+            options: [['Beginner', 'Intermediate', 'Advanced']],
+            errorMessage: 'Invalid Skills Proficiency',
+        },
+    },
 };
 
 export const createNewJobPost = async (
-  jobPostData: JobInput,
+    jobPostData: JobInput,
 ): Promise<DBJob> => {
-  const PromiseArray: Array<Promise<[RowDataPacket[], FieldPacket[]]>> = [];
-  const jobData: DBJob = {
-    jobId: uuidv4(),
-    companyId: jobPostData.companyId,
-    title: jobPostData.title,
-    description: jobPostData.description || null,
-    vacancies: jobPostData.vacancies,
-    experience: jobPostData.experience || null,
-    address: jobPostData.address || null,
-    district: jobPostData.district || null,
-  };
+    const PromiseArray: Array<Promise<[RowDataPacket[], FieldPacket[]]>> = [];
+    const jobData: DBJob = {
+        jobId: uuidv4(),
+        companyId: jobPostData.companyId,
+        title: jobPostData.title,
+        description: jobPostData.description || null,
+        vacancies: jobPostData.vacancies,
+        experience: jobPostData.experience || null,
+        address: jobPostData.address || null,
+        district: jobPostData.district || null,
+    };
 
-  const { qualifications, skills } = jobPostData;
-  await connection.execute(
-    'INSERT INTO jobs ' +
-      '(jobId, companyId, title, description, vacancies, experience, address, district) ' +
-      'VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-    [...Object.values(jobData)],
-  );
+    const { qualifications, skills } = jobPostData;
+    await connection.execute(
+        'INSERT INTO jobs '
+        + '(jobId, companyId, title, description, vacancies, experience, address, district) '
+        + 'VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+        [...Object.values(jobData)],
+    );
 
-  const baseQuery = 'INSERT INTO job_qualifications (jobId, qid) VALUES ';
-  let parameters: string[] = [];
-  let query = baseQuery;
-  qualifications.forEach((qualification, index) => {
-    // Batch qualifications into groups of 20
-    // for 0-19 index/20 gives 0 and for 20-39 index/20 gives 1 and so on
-    // at index 20, 1(from division) != 0(from length) so we insert into array
-    // This is to avoid the max bytes limitation of mysql
-    if (
-      Math.floor(index / 20) === PromiseArray.length &&
-      index !== qualifications.length - 1
-    ) {
-      query += '(?, ?), ';
-      parameters.push(jobData.jobId, qualification);
-    } else {
-      query += '(?, ?)';
-      parameters.push(jobData.jobId, qualification);
-      PromiseArray.push(connection.query(query, parameters));
-      query = baseQuery;
-      parameters = [];
+    const baseQuery = 'INSERT INTO job_qualifications (jobId, qid) VALUES ';
+    let parameters: string[] = [];
+    let query = baseQuery;
+    qualifications.forEach((qualification, index) => {
+        // Batch qualifications into groups of 20
+        // for 0-19 index/20 gives 0 and for 20-39 index/20 gives 1 and so on
+        // at index 20, 1(from division) != 0(from length) so we insert into array
+        // This is to avoid the max bytes limitation of mysql
+        if (
+            Math.floor(index / 20) === PromiseArray.length
+            && index !== qualifications.length - 1
+        ) {
+            query += '(?, ?), ';
+            parameters.push(jobData.jobId, qualification);
+        } else {
+            query += '(?, ?)';
+            parameters.push(jobData.jobId, qualification);
+            PromiseArray.push(connection.query(query, parameters));
+            query = baseQuery;
+            parameters = [];
+        }
+    });
+
+    const skillsBaseQuery = 'INSERT INTO skills (jobId, skillName, proficiency) VALUES ';
+    parameters = [];
+    query = skillsBaseQuery;
+    const initialPromiseArrayLength = PromiseArray.length;
+    skills.forEach((skill, index) => {
+        // Batch qualifications into groups of 20
+        // for 0-19 index/20 gives 0 and for 20-39 index/20 gives 1 and so on
+        // at index 20, 1(from division) != 0(from length) so we insert into array
+        // This is to avoid the max bytes limitation of mysql
+        if (
+            Math.floor(index / 20)
+            === PromiseArray.length - initialPromiseArrayLength
+            && index !== skills.length - 1
+        ) {
+            query += '(?, ?, ?), ';
+            parameters.push(jobData.jobId, skill.skillName, skill.proficiency);
+        } else {
+            query += '(?, ?, ?)';
+            parameters.push(jobData.jobId, skill.skillName, skill.proficiency);
+            PromiseArray.push(connection.query(query, parameters));
+            query = skillsBaseQuery;
+            parameters = [];
+        }
+    });
+    try {
+        await Promise.all(PromiseArray);
+    } catch (error) {
+        logger.error(error);
     }
-  });
-
-  const skillsBaseQuery =
-    'INSERT INTO skills (jobId, skillName, proficiency) VALUES ';
-  parameters = [];
-  query = skillsBaseQuery;
-  const initialPromiseArrayLength = PromiseArray.length;
-  skills.forEach((skill, index) => {
-    // Batch qualifications into groups of 20
-    // for 0-19 index/20 gives 0 and for 20-39 index/20 gives 1 and so on
-    // at index 20, 1(from division) != 0(from length) so we insert into array
-    // This is to avoid the max bytes limitation of mysql
-    if (
-      Math.floor(index / 20) ===
-        PromiseArray.length - initialPromiseArrayLength &&
-      index !== skills.length - 1
-    ) {
-      query += '(?, ?, ?), ';
-      parameters.push(jobData.jobId, skill.skillName, skill.proficiency);
-    } else {
-      query += '(?, ?, ?)';
-      parameters.push(jobData.jobId, skill.skillName, skill.proficiency);
-      PromiseArray.push(connection.query(query, parameters));
-      query = skillsBaseQuery;
-      parameters = [];
-    }
-  });
-  try {
-    await Promise.all(PromiseArray);
-  } catch (error) {
-    logger.error(error);
-  }
-  return { ...(<DBJob>jobData) };
+    return { ...(<DBJob>jobData) };
 };
 
 export const deleteJobPost = async (jobId: string) => {
-  try {
-    await connection.execute('DELETE FROM jobs ' + 'WHERE jobId = ?', [jobId]);
-    await connection.execute('DELETE FROM skills ' + 'WHERE jobId = ?', [
-      jobId,
-    ]);
-    await connection.execute(
-      'DELETE FROM job_qualifications ' + 'WHERE jobId = ?',
-      [jobId],
-    );
-  } catch (error) {
-    logger.error('Error deleting jobs', error);
-  }
+    try {
+        await connection.execute(
+            'CALL deleteJobs(?)',
+            [jobId],
+        );
+    } catch (error) {
+        logger.error('Error deleting jobs', error);
+    }
 };
