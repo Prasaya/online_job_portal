946552b4f46302fde186d7c76ac79c394c07f2d3
Suven-p <pandeysuven@gmail.com>
2022-02-17 17:54:30 +0545

    Fix react console errors

--- src/client/src/Components/Jobseeker/Nav.js
+++ src/client/src/Components/Jobseeker/Nav.js
@@ -7,7 +7,7 @@ function Nav(){
         {
             name: "Overview",
             to:"overview",
-            status: "" 
+            status: ""
         },
         {
             name: "My Status",
@@ -58,14 +58,14 @@ function Nav(){
             <div className="container-fluid">
                 <Link className="navbar-brand left" to="/">Job Portal</Link>
                 <div className="right">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-right" aria-controls="nav-right" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-right" aria-controls="nav-right" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="nav-right">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 {navElements.map((element) => {
                                     return(
-                                        <li className="nav-link">
+                                        <li className="nav-link" key={element.name}>
                                             <Link onClick={OnClick} className={`nav-link ${element.status}`} to={element.to} id={`${element.to}`}>{element.name}</Link>
                                         </li>
                                     )
