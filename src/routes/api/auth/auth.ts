7da4e5e9bdf9238eae85ef475023924ba11d1dad
Suven-p <pandeysuven@gmail.com>
2022-02-18 17:49:16 +0545

    Delete failed route

--- src/routes/api/auth/auth.ts
+++ src/routes/api/auth/auth.ts
@@ -9,14 +9,6 @@ router.use('/google', googleHandler);
 router.use('/login', localLoginHandler);
 router.use('/register', localRegisterHandler);
 
-router.get('/failed', (req, res) => {
-  res.json({
-    message: 'Failed to authenticate',
-    log: req.session.messages,
-    success: false,
-  });
-});
-
 router.post('/logout', (req, res) => {
   req.logout();
   res.json({ message: 'Logout successful!', success: true });
