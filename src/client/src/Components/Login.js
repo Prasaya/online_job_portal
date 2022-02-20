262a77c61ce83d4dced0a8a9db484d3e4b1ce26e
Suprim Shrestha <suprim.shresth@gmail.com>
2022-02-20 21:59:31 +0545

    Fix link to register pages

--- src/client/src/Components/Login.js
+++ src/client/src/Components/Login.js
@@ -57,8 +57,8 @@ function Login(){
                     </form>
                     <hr />
                     <div className="d-grid gap-2">
-                        <Link to="register-jobseeker" className="btn btn-success btn-lg">Create new job seeker account</Link>
-                        <Link to="register-company" className="btn btn-success btn-lg">Create new employer account</Link>
+                        <Link to="/register-jobseeker" className="btn btn-success btn-lg">Create new job seeker account</Link>
+                        <Link to="/register-company" className="btn btn-success btn-lg">Create new employer account</Link>
                     </div>
                 </div>
             </div>
