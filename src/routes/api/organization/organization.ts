6ce5d657294e498353653af437360cc92972f36f
Prasaya <prasaya.acharya@gmail.com>
2022-02-20 23:02:36 +0545

    Refactor Organization Job Fetching into function

--- src/routes/api/organization/organization.ts
+++ src/routes/api/organization/organization.ts
@@ -15,18 +15,23 @@ router.get('/', (req, res) => {
   });
 });
 
+export const fetchOrganizationJobs = async (companyId: string) => {
+  const [result] = await connection.execute('CALL getCompanyJobsData(?)', [companyId]);
+  console.log(result);
+  if (Array.isArray(result)) {
+    return { jobList: result[0], success: true };
+  }
+  return { jobList: [], success: false };
+}
+
 // TODO: Add/Merge applicant 
 router.get(
   '/jobs',
   async (req, res) => {
     try {
-      const companyID = req.user?.user.basics.id;
-      const [result] = await connection.execute('CALL getCompanyJobsData(?)', [companyID]);
-      if (Array.isArray(result)) {
-        res.json(result[0]);
-        return;
-      }
-      res.json([]);
+      const companyID = req.user!.user.basics.id;
+      const result = (await fetchOrganizationJobs(companyID));
+      res.json(result);
     } catch (err) {
       logger.error('Error in getting all jobs posted by company', err);
       res
