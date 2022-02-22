08eb5c7afbccade85678ae27b358563b779afb19
maskudo <manujkarki101@gmail.com>
2022-02-22 23:25:02 +0545

    add company profile page

--- src/client/src/Components/Company/Profile.js
+++ src/client/src/Components/Company/Profile.js
@@ -1,7 +1,80 @@
+import { useState, useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import defaultAvatar from "../../Assets/Img/defaultAvatar.png"
+
+function titleCase(str) {
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map(function (word) {
+      return word.replace(word[0], word[0].toUpperCase());
+    })
+    .join(' ');
+}
 function Profile() {
+  const [basicInfo, setBasicInfo] = useState({});
+  let basicEntries = [];
+  const notDisplay = ['id', 'roles', 'type', 'logo'];
+
+  const fetchInfo = async () => {
+    const res = await fetch('http://localhost:4000/company');
+    const data = await res.json();
+    return data;
+  };
+
+  useEffect(() => {
+    const getInfo = async () => {
+      const user = await fetchInfo();
+      setBasicInfo(user.basics);
+    };
+    getInfo();
+  }, []);
+  basicEntries = Object.entries(basicInfo);
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="myprofile">
+      <h1>
+        My Profile{' '}
+        <span>
+          <Link className="btn btn-secondary" to="/company/editprofile">
+            Edit
+          </Link>
+        </span>
+      </h1>
+      <div className="container bg-light profile">
+        <div className="row">
+          <div className="col-10 col-sm-2">
+            <img
+              className="img-fluid img-thumbnail rounded float-start"
+              src={basicInfo.logo || defaultAvatar}
+              alt="profile pic"
+            />
+          </div>
+          <div className="col-10">
+            <div className="row">
+              <div className="basic col-lg-6">
+                <h3>Basic information</h3>
+                <table className="table">
+                  <tbody>
+                    {basicEntries.map((entry) => {
+                      return (
+                        <Fragment key={entry[0]}>
+                          {entry[1] && !notDisplay.includes(entry[0].toLowerCase()) && (
+                            <tr>
+                              <td>{titleCase(entry[0])}</td>
+                              <td>:</td>
+                              <td>{entry[1]}</td>
+                            </tr>
+                          )}
+                        </Fragment>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
