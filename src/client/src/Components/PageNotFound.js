671984d22113b5c0d4ee66a380a8b1044889bda9
maskudo <manujkarki101@gmail.com>
2022-02-23 22:40:38 +0545

    add spacing

--- src/client/src/Components/PageNotFound.js
+++ src/client/src/Components/PageNotFound.js
@@ -6,7 +6,7 @@ function PageNotFount() {
                 <img src="https://png.pngtree.com/png-vector/20191022/ourlarge/pngtree-question-mark-businessman-vector-task-hr-concept-find-new-job-huge-png-image_1841501.jpg" alt="error:404" />
             </div>
             <div className="col-4">
-                <h1 className="pb-3">Error:404</h1>
+                <h1 className="pb-3">Error : 404</h1>
                 <h1 className="pb-5">Page Not Found</h1>
                 <Link to="/" className="btn btn-primary">
                     Go to Homepage
