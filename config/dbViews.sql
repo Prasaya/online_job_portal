bd2ec0182f2da2a8782085914f2ed3780da704bf
Prasaya <prasaya.acharya@gmail.com>
2022-02-23 16:07:09 +0545

    Add view to get all jobs from database

---
+++ config/dbViews.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE VIEW allJobsFromDatabase AS
+SELECT j.jobId, j.companyId, j.title, j.description, j.vacancies, j.experience, j.address,
+	(SELECT
+		JSON_ARRAYAGG(
+			JSON_OBJECT('name', s.skillName, 'proficiency', s.proficiency)
+		)
+		FROM skills AS s
+        WHERE s.jobId = j.jobId
+		GROUP BY s.jobId
+	) AS skills,
+	(SELECT
+		JSON_ARRAYAGG(
+			JSON_OBJECT('qid', q.qid, 'level', aq.level, 'discipline', aq.discipline, 'degree', aq.degree)
+		)	
+		FROM job_qualifications AS q 
+		INNER JOIN jobs AS jb on jb.jobId = q.jobId
+		INNER JOIN academic_qualifications AS aq ON q.qid = aq.qid
+		WHERE q.jobId = j.jobId 
+		GROUP BY q.jobId
+	) as qualifications
+FROM jobs as j
+GROUP BY j.jobId;
