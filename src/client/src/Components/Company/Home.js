a6f4166213055a91fd1d9bebffe52a9fb7dc9168
Suprim Shrestha <suprim.shresth@gmail.com>
2022-02-23 23:46:03 +0545

    check for auth status from home not header

--- src/client/src/Components/Company/Home.js
+++ src/client/src/Components/Company/Home.js
@@ -1,7 +1,26 @@
-import { Outlet } from 'react-router-dom';
-import Nav from './CompanyNav';
+import { useContext, useEffect } from 'react';
+import { useLocation, useNavigate, Outlet } from 'react-router-dom';
+import UserContext from '../../Context/UserContext';
 
 function Home() {
+  const path = useLocation().pathname;
+  const userCtx = useContext(UserContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (userCtx.authStatus) {
+      if (userCtx.type === 'Organizations' && path.startsWith('/jobseeker')) {
+        navigate('/company/overview', { replace: true });
+      } else if (userCtx.type === 'Users' && path.startsWith('/company')) {
+        navigate('/jobseeker/overview', { replace: true });
+      }
+    } else if (
+      !userCtx.authStatus &&
+      (path.startsWith('/company') || path.startsWith('/jobseeker'))
+    ) {
+      navigate('/login', { replace: true });
+    }
+  }, []);
 
   return (
     <>
--- src/client/src/Components/Header.js
+++ src/client/src/Components/Header.js
@@ -2,29 +2,11 @@ import JobSeekerNav from './Jobseeker/Nav';
 import CompanyNav from './Company/CompanyNav';
 import DefaultNav from './DefaultNav';
 
-import { useContext, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
 import UserContext from '../Context/UserContext';
 
 function Header() {
-  const path = useLocation().pathname;
   const userCtx = useContext(UserContext);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (userCtx.authStatus) {
-      if (userCtx.type === 'Organizations' && path.startsWith('/jobseeker')) {
-        navigate('/company/overview', { replace: true });
-      } else if (userCtx.type === 'Users' && path.startsWith('/company')) {
-        navigate('/jobseeker/overview', { replace: true });
-      }
-    } else if (
-      !userCtx.authStatus &&
-      (path.startsWith('/company') || path.startsWith('/jobseeker'))
-    ) {
-      navigate('/login', { replace: true });
-    }
-  }, []);
 
   if (userCtx.type === 'Users') {
     return <JobSeekerNav />;
