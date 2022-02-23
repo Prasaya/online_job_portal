34d96abb716a88977566041bbbc3c5d893e5dedc
Prasaya <prasaya.acharya@gmail.com>
2022-02-23 19:36:32 +0545

    Add applicant put api ko update user details

--- src/routes/api/applicant/applicant.ts
+++ src/routes/api/applicant/applicant.ts
@@ -1,9 +1,13 @@
 import { isApplicant } from '@middleware/authorization';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import academicsRoute from '../applicant/academics';
 import avatarRoute from './avatar';
 import jobsRoute from './jobs';
 import skillsRoute from './skills';
+import { checkSchema, validationResult } from 'express-validator';
+import { updateUser, updateUserSchema } from '@models/User';
+import { UpdateUser } from '@typings/User';
+import logger from '@utils/logger';
 
 const router = express.Router();
 router.use(isApplicant);
@@ -20,6 +24,37 @@ router.get('/', (req, res) => {
   });
 });
 
-router.put('/', (req, res) => {});
+router.put(
+  '/',
+  checkSchema(updateUserSchema),
+  async (req: Request, res: Response) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res
+          .status(400)
+          .json({ message: errors.array(), success: false });
+      }
+
+      let uId = req.user!.user.basics.id;
+      const userData: UpdateUser = {
+        id: req.user!.user.basics.id,
+        firstName: req.body.firstName || null,
+        middleName: req.body.middleName || null,
+        lastName: req.body.lastName || null,
+        birthday: req.body.birthday || null,
+        phone: req.body.phone || null,
+        gender: req.body.gender || null,
+      };
+      const result = await updateUser(userData);
+      return res.json({ result, success: true });
+    } catch (err) {
+      logger.error(`Error in changing user details: ${err}`);
+      return res
+        .status(500)
+        .json({ message: 'Something went wrong!', success: false });
+    }
+  },
+);
 
 export default router;
