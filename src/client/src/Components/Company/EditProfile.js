71b30d659f87627adfb57bc2437403cd0cb8df09
maskudo <manujkarki101@gmail.com>
2022-02-23 17:42:19 +0545

    refactor edit profile to look like jobseeker's

--- src/client/src/Components/Company/EditProfile.js
+++ src/client/src/Components/Company/EditProfile.js
@@ -1,154 +1,76 @@
-import { useForm } from 'react-hook-form';
-import { useEffect, useState } from 'react';
-//todo:avatar edit
+import {useState, useEffect} from "react"
+import {Link, useLocation, Outlet} from "react-router-dom"
+
+
 function EditProfile() {
-    const [basicInfo, setBasicInfo] = useState({});
-  const [allInfo, setAllInfo] = useState({}); //becasuse json server doesnt allow directly nested objects
-  const {
-    register,
-    reset,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ mode: 'onBlur' });
-
-  const fetchInfo = async () => {
-    const res = await fetch('http://localhost:4000/company');
-    const data = await res.json();
-    return data;
-  };
-
-  const onSubmitForm = (data) => {
-    let info = allInfo;
-    info.basics = data;
-    fetch(`http://localhost:4000/company`, {
-      method: 'PUT',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(info),
-    });
-  };
-
-  useEffect(() => {
-    const getInfo = async () => {
-      const info = await fetchInfo();
-      setAllInfo(info);
-      setBasicInfo(info['basics']);
-    };
-    getInfo();
-  }, []);
-
-  useEffect(() => {
-    reset(basicInfo);
-  }, [basicInfo, reset]);
-
-  return (
-    <div className="">
-        <h3>Basics</h3>
-        <div className="row">
-            <div className="col-6 avatar">
-
-            </div>
-            <div className="col-lg-6 m-auto mb-2">
-                <form onSubmit={handleSubmit(onSubmitForm)}>
-                    <div className="form-floating mb-3">
-                    <input
-                        {...register('name', {
-                        required: 'Please fill out this field',
-                        })}
-                        type="text"
-                        className={`form-control form-control-lg ${
-                        errors.name ? 'is-invalid' : ''
-                        }`}
-                        placeholder="Name"
-                        id="name"
-                    />
-                    <label htmlFor="name">Name</label>
-                    <div className="invalid-feedback">
-                        {errors.name && errors.name.message}
+    const path = useLocation().pathname
+    const [navElements, setNavElements] = useState([
+        {
+            name: "Avatar",
+            to:"avatar",
+            status: "" 
+        },
+        {
+            name: "Basics",
+            to:"basics",
+            status: ""
+        }]
+    )
+    function OnClick(e){
+        const id = e.target.id
+        const updatedNavElements = navElements.map((element) => {
+            if (element.status === "table-active"){
+                element.status = ""
+            }
+            if (element.to === id){
+                element.status = "table-active"
+            }
+            return element
+        })
+        setNavElements(updatedNavElements)
+    }
+    //set active for initial render
+    useEffect(() => {
+        const updatedNavElements = navElements.map((element) => {
+            if (element.status === "table-active"){
+                element.status = ""
+            }
+            if (path.includes(element.to) ){
+                element.status = "table-active"
+            }
+            return element
+        })
+        setNavElements(updatedNavElements)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    
+    return (
+        <div className="editprofile">
+            <h1>Edit Profile</h1>
+            <div className="bg-light container">
+                <div className="row">
+                    <div className="col-12 col-lg-3">
+                        <table className="table table-hover">
+                            <tbody>
+                                {navElements.map((element) => {
+                                    return(
+                                        <tr className="">
+                                            <td className={`${element.status}`}>
+                                                <Link onClick={OnClick} className={`nav-link link-dark`} to={element.to} id={`${element.to}`}>{element.name}</Link>
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </table>
                     </div>
+                    <div className="col-12 col-lg-9">
+                        <Outlet/>
                     </div>
-                    <div className="form-floating mb-3">
-                    <input
-                        {...register('address', {
-                        required: 'Please fill out this field',
-                        })}
-                        type="text"
-                        className={`form-control form-control-lg ${
-                        errors.city ? 'is-invalid' : ''
-                        }`}
-                        placeholder="Address"
-                        id="address"
-                    />
-                    <label htmlFor="address">Address</label>
-                    </div>
-                    <div className="form-floating mb-3">
-                    <input
-                        {...register('city', {
-                        required: 'Please fill out this field',
-                        })}
-                        type="text"
-                        className={`form-control form-control-lg ${
-                        errors.city ? 'is-invalid' : ''
-                        }`}
-                        placeholder="City"
-                        id="city"
-                    />
-                    <label htmlFor="city">City</label>
-                    <div className="invalid-feedback">
-                        {errors.city && errors.city.message}
-                    </div>
-                    </div>
-
-                    <div className="form-floating mb-3">
-                    <input
-                        {...register('phone', {
-                        pattern: {
-                            value: /^[0-9\b]+$/,
-                            message: 'Phone number must be a number',
-                        },
-                        required: 'Please fill out this field',
-                        })}
-                        type="text"
-                        className={`form-control form-control-lg ${
-                        errors.phone ? 'is-invalid' : ''
-                        }`}
-                        placeholder="Phone Number"
-                        id="phoneNumber"
-                    />
-                    <label htmlFor="phoneNumber">Phone Number</label>
-                    <div className="invalid-feedback">
-                        {errors.phone && errors.phone.message}
-                    </div>
-                    </div>
-                    <div className="form-floating mb-3">
-                    <input
-                        {...register('website', {
-                        required: 'Please fill out this field',
-                        })}
-                        type="website"
-                        className={`form-control form-control-lg ${
-                        errors.city ? 'is-invalid' : ''
-                        }`}
-                        placeholder="https://mywebsite.com"
-                        id="website"
-                    />
-                    <label htmlFor="website">Website</label>
-                    <div className="invalid-feedback">
-                        {errors.website && errors.website.message}
-                    </div>
-                    </div>
-                    
-                    <div className="d-grid gap-2">
-                    <button className="btn btn-success btn-lg" type="submit">
-                        Update
-                    </button>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
-    </div>
-  );
+    )
 }
 
 export default EditProfile;
