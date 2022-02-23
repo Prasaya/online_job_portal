92e4f1e43da0aad302882d3024e5389b44e635e9
Suprim Shrestha <suprim.shresth@gmail.com>
2022-02-23 23:46:29 +0545

    fetch job using job id

--- src/client/src/Components/Job.js
+++ src/client/src/Components/Job.js
@@ -1,8 +1,40 @@
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function Job() {
-    const { id } = useParams();
-  return <>Job {id}</>;
+  const [job, setJob] = useState();
+  const { id } = useParams();
+  const [isLoading, setLoading] = useState(true);
+  const [isValid, setValid] = useState(false);
+
+  async function fetchJobs() {
+    const res = await fetch(`/api/jobs/${id}`);
+    const data = await res.json();
+    return data;
+  }
+
+  useEffect(() => {
+    async function getJobs() {
+      const overviewJobs = await fetchJobs();
+      if(overviewJobs.success) {
+        setJob(overviewJobs.jobDetails);
+        setValid(true);
+      } else {
+        setValid(false);
+      }
+      setLoading(false);
+    }
+    getJobs();
+  }, []);
+
+
+  if(isLoading) {
+    return <div className="container"><h6>Loading...</h6></div>
+  }
+  if(!isValid) {
+    return <div className="container"><h6>Job doesn't exist.</h6></div>
+  }
+  return <div className="container job">Something {job.companyName}</div>;
 }
 
 export default Job;
