ef74ff1163c4194aa626b91c633316dee10316d1
maskudo <manujkarki101@gmail.com>
2022-02-24 16:50:10 +0545

    add routing for change email and password settings

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -12,6 +12,8 @@ import JobseekerOverview from "./Components/Jobseeker/Overview";
 import JobseekerMyStatus from "./Components/Jobseeker/MyStatus";
 import JobseekerMyProfile from "./Components/Jobseeker/MyProfile";
 import JobseekerSettings from "./Components/Jobseeker/Settings";
+import JobseekerChangeEmail from "./Components/Jobseeker/Settings/ChangeEmail"
+import JobseekerChangePassword from "./Components/Jobseeker/Settings/ChangePassword"
 import JobseekerEditProfile from "./Components/Jobseeker/EditProfile";
 import JobseekerAvatar from "./Components/Jobseeker/EditProfile/Avatar";
 import JobseekerBasics from "./Components/Jobseeker/EditProfile/Basics";
@@ -49,7 +51,10 @@ function App() {
               <Route path="education" element={<JobseekerEducation/>}/>
               <Route path="skills" element={<JobseekerSkills/>}/>
             </Route>
-            <Route path="settings" element={<JobseekerSettings/>} />
+            <Route path="settings" element={<JobseekerSettings/>} >
+              <Route path="email" element={<JobseekerChangeEmail/>}/>
+              <Route path="password" element={<JobseekerChangePassword/>}/>
+            </Route>
           </Route>
           <Route path="/company" element={<CompanyHome />}>
             <Route path="overview" element={<CompanyOverview/>} />
