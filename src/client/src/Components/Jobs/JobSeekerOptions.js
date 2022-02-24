e472fb82e41f55d7b7b66ebe41ae483a04f963ce
Suprim Shrestha <suprim.shresth@gmail.com>
2022-02-24 17:54:05 +0545

    Check job applied status

--- src/client/src/Components/Jobs/JobSeekerOptions.js
+++ src/client/src/Components/Jobs/JobSeekerOptions.js
@@ -1,7 +1,25 @@
 import { useEffect, useState } from 'react';
 
 function JobSeekerOptions({ id }) {
-  const [jobApplied, setApplied] = useState();
+  const [jobApplied, setApplied] = useState(false);
+
+  async function fetchJobs() {
+    const res = await fetch('/api/applicant/jobs');
+    const data = await res.json();
+    return data;
+  }
+
+  useEffect(() => {
+    async function getJobs() {
+      const overviewJobs = await fetchJobs();
+      overviewJobs.jobs.map((job) => {
+        if (id.jobId === job.jobId) {
+          setApplied(true);
+        }
+      });
+    }
+    getJobs();
+  }, []);
 
   const jobApply = async (e) => {
     e.preventDefault();
@@ -13,13 +31,15 @@ function JobSeekerOptions({ id }) {
       body: JSON.stringify(id),
     });
     const jsonVal = await res.json();
-    console.log(jsonVal);
+    if (jsonVal.success) {
+      setApplied(true);
+    }
   };
 
   return (
     <div className="container">
       <button className="btn btn-primary" onClick={jobApply}>
-        Apply
+        {jobApplied ? 'Unapply' : 'Apply'}
       </button>
     </div>
   );
