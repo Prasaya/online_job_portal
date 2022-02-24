9955eec6a824d0a0cdb5a71eee5c01fbec949396
Suprim Shrestha <suprim.shresth@gmail.com>
2022-02-24 14:39:59 +0545

    Display job details

--- src/client/src/Components/Job.js
+++ src/client/src/Components/Job.js
@@ -16,7 +16,7 @@ function Job() {
   useEffect(() => {
     async function getJobs() {
       const overviewJobs = await fetchJobs();
-      if(overviewJobs.success) {
+      if (overviewJobs.success) {
         setJob(overviewJobs.jobDetails);
         setValid(true);
       } else {
@@ -27,14 +27,93 @@ function Job() {
     getJobs();
   }, []);
 
-
-  if(isLoading) {
-    return <div className="container"><h6>Loading...</h6></div>
+  if (isLoading) {
+    return (
+      <div className="container">
+        <h6>Loading...</h6>
+      </div>
+    );
   }
-  if(!isValid) {
-    return <div className="container"><h6>Job doesn't exist.</h6></div>
+  if (!isValid) {
+    return (
+      <div className="container">
+        <h6>Job doesn't exist.</h6>
+      </div>
+    );
   }
-  return <div className="container job">Something {job.companyName}</div>;
+  return (
+    <div className="container job">
+      <h2>{job.title}</h2>
+      <div className="job-details mb-4">
+        <table className="my-1 table table-borderless">
+          <tbody>
+            <tr>
+              <td>
+                <b>Company</b>
+              </td>
+              <td className="px-0">:</td>
+              <td>{job.companyName}</td>
+            </tr>
+            <tr>
+              <td>
+                <b>Address</b>
+              </td>
+              <td className="px-0">:</td>
+              <td>
+                {job.address} {job.district}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <b>Deadline</b>
+              </td>
+              <td className="px-0">:</td>
+              <td>2022-03-10</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="job-description my-2">
+          <h6>Job Description:</h6>
+          <p>{job.description}</p>
+        </div>
+      </div>
+      <div className="job-requirements">
+        <h5>Job Requirements:</h5>
+        <p>Experience: {job.experience}</p>
+        <div className="my-3 education">
+          <h6>Education:</h6>
+          <ul className="list-group">
+            {job.qualifications.map((education) => {
+              return <li className="list-group-item">{education.degree}</li>;
+            })}
+          </ul>
+        </div>
+        <div className="my-3 skills">
+          <h6>Skills:</h6>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Skill Name</th>
+                <th scope="col">Proficiency</th>
+              </tr>
+            </thead>
+            <tbody>
+              {job.skills.map((skill, index) => {
+                return (
+                  <tr key={skill.id}>
+                    <td>{index + 1}</td>
+                    <td>{skill.name}</td>
+                    <td>{skill.proficiency}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Job;
