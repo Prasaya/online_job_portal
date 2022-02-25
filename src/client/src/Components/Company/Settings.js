3317e0bcb8cbb57ca1412c5f34dda171dad488da
maskudo <manujkarki101@gmail.com>
2022-02-25 19:22:21 +0545

    add company settings

--- src/client/src/Components/Company/Settings.js
+++ src/client/src/Components/Company/Settings.js
@@ -1,9 +1,76 @@
+import {useState, useEffect} from "react"
+import {Link, useLocation, Outlet} from "react-router-dom"
+
+
 function Settings() {
-  return (
-    <div>
-      <h1>Settings</h1>
-    </div>
-  );
+    const path = useLocation().pathname
+    const [navElements, setNavElements] = useState([
+        {
+            name: "Email",
+            to:"email",
+            status: "" 
+        },
+        {
+            name: "Password",
+            to:"password",
+            status: ""
+        }]
+    )
+    function OnClick(e){
+        const id = e.target.id
+        const updatedNavElements = navElements.map((element) => {
+            if (element.status === "table-active"){
+                element.status = ""
+            }
+            if (element.to === id){
+                element.status = "table-active"
+            }
+            return element
+        })
+        setNavElements(updatedNavElements)
+    }
+    //set active for initial render
+    useEffect(() => {
+        const updatedNavElements = navElements.map((element) => {
+            if (element.status === "table-active"){
+                element.status = ""
+            }
+            if (path.includes(element.to) ){
+                element.status = "table-active"
+            }
+            return element
+        })
+        setNavElements(updatedNavElements)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    
+    return (
+        <div className="settings">
+            <h1>Settings</h1>
+            <div className="bg-light container">
+                <div className="row">
+                    <div className="col-12 col-lg-3">
+                        <table className="table table-hover">
+                            <tbody>
+                                {navElements.map((element) => {
+                                    return(
+                                        <tr className="">
+                                            <td className={`${element.status}`}>
+                                                <Link onClick={OnClick} className={`nav-link link-dark`} to={element.to} id={`${element.to}`}>{element.name}</Link>
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="col-12 col-lg-9">
+                        <Outlet/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Settings;
