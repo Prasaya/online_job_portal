0af51cf894f51f697e1094ded0865999d93b5023
Suprim Shrestha <suprim.shresth@gmail.com>
2022-02-26 13:44:58 +0545

    Add District Component

---
+++ src/client/src/Components/District.js
@@ -0,0 +1,135 @@
+function District({ form, errors }) {
+  return (
+    <select
+      {...form.register('district', {
+        required: 'Please choose a valid option',
+      })}
+      className={`form-select ${errors ? 'is-invalid' : ''}`}
+      id="district"
+    >
+      <option selected disabled value="">
+        Choose...
+      </option>
+      <optgroup label="Mechi">
+        <option value="Taplejung">Taplejung</option>
+        <option value="Panchthar">Panchthar</option>
+        <option value="Ilam">Ilam</option>
+        <option value="Jhapa">Jhapa</option>
+      </optgroup>
+
+      <optgroup label="Koshi">
+        <option value="Morang">Morang</option>
+        <option value="Sunsari">Sunsari</option>
+        <option value="Dhankutta">Dhankutta</option>
+        <option value="Sankhuwasabha">Sankhuwasabha</option>
+        <option value="Bhojpur">Bhojpur</option>
+        <option value="Terhathum">Terhathum</option>
+      </optgroup>
+
+      <optgroup label="Sagarmatha">
+        <option value="Okhaldunga">Okhaldunga</option>
+        <option value="Khotang">Khotang</option>
+        <option value="Solukhumbu">Solukhumbu</option>
+        <option value="Udaypur">Udaypur</option>
+        <option value="Saptari">Saptari</option>
+        <option value="Siraha">Siraha</option>
+      </optgroup>
+
+      <optgroup label="Janakpur">
+        <option value="Dhanusa">Dhanusa</option>
+        <option value="Mahottari">Mahottari</option>
+        <option value="Sarlahi">Sarlahi</option>
+        <option value="Sindhuli">Sindhuli</option>
+        <option value="Ramechhap">Ramechhap</option>
+        <option value="Dolkha">Dolkha</option>
+      </optgroup>
+
+      <optgroup label="Bagmati">
+        <option value="Sindhupalchauk">Sindhupalchauk</option>
+        <option value="Kavreplanchauk">Kavreplanchauk</option>
+        <option value="Lalitpur">Lalitpur</option>
+        <option value="Bhaktapur">Bhaktapur</option>
+        <option value="Kathmandu">Kathmandu</option>
+        <option value="Nuwakot">Nuwakot</option>
+        <option value="Rasuwa">Rasuwa</option>
+        <option value="Dhading">Dhading</option>
+      </optgroup>
+
+      <optgroup label="Narayani">
+        <option value="Makwanpur">Makwanpur</option>
+        <option value="Rauthat">Rauthat</option>
+        <option value="Bara">Bara</option>
+        <option value="Parsa">Parsa</option>
+        <option value="Chitwan">Chitwan</option>
+      </optgroup>
+
+      <optgroup label="Gandaki">
+        <option value="Gorkha">Gorkha</option>
+        <option value="Lamjung">Lamjung</option>
+        <option value="Tanahun">Tanahun</option>
+        <option value="Tanahun">Tanahun</option>
+        <option value="Syangja">Syangja</option>
+        <option value="Kaski">Kaski</option>
+        <option value="Manag">Manag</option>
+      </optgroup>
+
+      <optgroup label="Dhawalagiri">
+        <option value="Mustang">Mustang</option>
+        <option value="Parwat">Parwat</option>
+        <option value="Myagdi">Myagdi</option>
+        <option value="Myagdi">Myagdi</option>
+        <option value="Baglung">Baglung</option>
+      </optgroup>
+
+      <optgroup label="Lumbini">
+        <option value="Gulmi">Gulmi</option>
+        <option value="Palpa">Palpa</option>
+        <option value="Nawalparasi">Nawalparasi</option>
+        <option value="Rupandehi">Rupandehi</option>
+        <option value="Arghakhanchi">Arghakhanchi</option>
+        <option value="Kapilvastu">Kapilvastu</option>
+      </optgroup>
+
+      <optgroup label="Rapti">
+        <option value="Pyuthan">Pyuthan</option>
+        <option value="Rolpa">Rolpa</option>
+        <option value="Rukum">Rukum</option>
+        <option value="Salyan">Salyan</option>
+        <option value="Dang">Dang</option>
+      </optgroup>
+
+      <optgroup label="Bheri">
+        <option value="Bardiya">Bardiya</option>
+        <option value="Surkhet">Surkhet</option>
+        <option value="Dailekh">Dailekh</option>
+        <option value="Banke">Banke</option>
+        <option value="Jajarkot">Jajarkot</option>
+      </optgroup>
+
+      <optgroup label="Karnali">
+        <option value="Dolpa">Dolpa</option>
+        <option value="Humla">Humla</option>
+        <option value="Kalikot">Kalikot</option>
+        <option value="Mugu">Mugu</option>
+        <option value="Jumla">Jumla</option>
+      </optgroup>
+
+      <optgroup label="Seti">
+        <option value="Bajura">Bajura</option>
+        <option value="Bajhang">Bajhang</option>
+        <option value="Achham">Achham</option>
+        <option value="Doti">Doti</option>
+        <option value="Kailali">Kailali</option>
+      </optgroup>
+
+      <optgroup label="Mahakali">
+        <option value="Kanchanpur">Kanchanpur</option>
+        <option value="Dadeldhura">Dadeldhura</option>
+        <option value="Baitadi">Baitadi</option>
+        <option value="Darchula">Darchula</option>
+      </optgroup>
+    </select>
+  );
+}
+
+export default District;
