0fe24c33c6f2f57f133197a4650383f332b0d652
Suprim Shrestha <suprim.shresth@gmail.com>
2022-02-28 16:44:39 +0545

    Uncomment redirection when not logged in

--- src/client/src/Components/Company/Home.js
+++ src/client/src/Components/Company/Home.js
@@ -7,20 +7,20 @@ function Home() {
   const userCtx = useContext(UserContext);
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   if (userCtx.authStatus) {
-  //     if (userCtx.type === 'Organizations' && path.startsWith('/jobseeker')) {
-  //       navigate('/company/overview', { replace: true });
-  //     } else if (userCtx.type === 'Users' && path.startsWith('/company')) {
-  //       navigate('/jobseeker/overview', { replace: true });
-  //     }
-  //   } else if (
-  //     !userCtx.authStatus &&
-  //     (path.startsWith('/company') || path.startsWith('/jobseeker'))
-  //   ) {
-  //     navigate('/login', { replace: true });
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (userCtx.authStatus) {
+      if (userCtx.type === 'Organizations' && path.startsWith('/jobseeker')) {
+        navigate('/company/overview', { replace: true });
+      } else if (userCtx.type === 'Users' && path.startsWith('/company')) {
+        navigate('/jobseeker/overview', { replace: true });
+      }
+    } else if (
+      !userCtx.authStatus &&
+      (path.startsWith('/company') || path.startsWith('/jobseeker'))
+    ) {
+      navigate('/login', { replace: true });
+    }
+  }, []);
 
   return (
     <>
