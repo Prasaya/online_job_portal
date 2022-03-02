4132c485d32dc5d04371f3015642071f51727847
Prasaya <prasaya.acharya@gmail.com>
2022-03-02 16:58:19 +0545

    Add url shortener for sending SMS

--- src/models/Sms.ts
+++ src/models/Sms.ts
@@ -1,6 +1,7 @@
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const phoneNumber = process.env.TWILIO_PHONE_NUMBER;
+const shortUrl = require('node-url-shortener');
 
 export const sendSms = (phone, message) => {
     const client = require('twilio')(accountSid, authToken);
