84a6ff76d6f3655136efa26987a1e270cfbdb6f1
Suprim Shrestha <suprim.shresth@gmail.com>
2022-03-05 17:15:25 +0545

    Add more info in statistics

--- src/client/src/Components/Company/Overview.js
+++ src/client/src/Components/Company/Overview.js
@@ -41,29 +41,55 @@ function Overview() {
                       View Job
                     </Link>
 
-                    <button type="button" class="btn btn-primary btn-lg mx-2" data-bs-toggle="modal" data-bs-target="#statisticModal">
+                    <button
+                      type="button"
+                      class="btn btn-primary btn-lg mx-2"
+                      data-bs-toggle="modal"
+                      data-bs-target="#statisticModal"
+                    >
                       View Statistics
                     </button>
 
-                    <div class="modal modal-dialog-scrollable fade" id="statisticModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+                    <div
+                      class="modal modal-dialog-scrollable fade"
+                      id="statisticModal"
+                      tabindex="-1"
+                      aria-labelledby="modalLabel"
+                      aria-hidden="true"
+                    >
                       <div class="modal-dialog modal-dialog-centered">
                         <div class="modal-content">
                           <div class="modal-header">
-                            <h5 class="modal-title" id="modalLabel"> {job.title} Statistics</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <h5 class="modal-title" id="modalLabel">
+                              {' '}
+                              {job.title} Statistics
+                            </h5>
+                            <button
+                              type="button"
+                              class="btn-close"
+                              data-bs-dismiss="modal"
+                              aria-label="Close"
+                            ></button>
                           </div>
                           <div class="modal-body">
-                            <p>Total Notifications Sent: {20}</p>
-                            <p>Total Notifications Viewed: {5}</p>
-                            <p>Applicants from Notifications: {7}</p>
+                            <p>Total Notifications Sent: {7}</p>
+                            <p>Total Notifications Viewed: {4}</p>
+                            <p>Job Post Viewed: {10}</p>
+                            <p>Applicants from Notifications: {3}</p>
+                            <p>Total Applicants: {5}</p>
                           </div>
                           <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button
+                              type="button"
+                              class="btn btn-secondary"
+                              data-bs-dismiss="modal"
+                            >
+                              Close
+                            </button>
                           </div>
                         </div>
                       </div>
                     </div>
-
                   </div>
                 </div>
               </li>
