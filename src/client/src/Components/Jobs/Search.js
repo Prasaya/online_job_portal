d901fc22e43d099cfc95b64966998fab146708e5
Suprim Shrestha <suprim.shresth@gmail.com>
2022-03-06 22:31:22 +0545

    Update job list with change of query

--- src/client/src/Components/Jobs/Search.js
+++ src/client/src/Components/Jobs/Search.js
@@ -17,35 +17,40 @@ function Search() {
       setJobs(overviewJobs.jobs);
     }
     getJobs();
-  }, []);
+  }, [searchParams]);
 
   return (
-    <div className="overview card my-2">
-      <h1 className="card-header">Overview</h1>
-      <div className="jobs container">
-        <ul className=" list-group">
-          {jobs.map((job) => {
-            return (
-              <li key={job.jobId} className="p-3 my-3 bg-light list-group-item">
-                <div className="job d-flex flex-row justify-content-between align-items-center my-2">
-                  <div className="d-flex flex-column">
-                    <h3>{job.title}</h3>
-                    <h6>Company: {job.companyName}</h6>
-                    <h6>Deadline: {job.deadline}</h6>
+    <div className="container">
+      <div className="overview card my-2">
+        <h3 className="card-header">Search Results</h3>
+        <div className="jobs container">
+          <ul className=" list-group">
+            {jobs.map((job) => {
+              return (
+                <li
+                  key={job.jobId}
+                  className="p-3 my-3 bg-light list-group-item"
+                >
+                  <div className="job d-flex flex-row justify-content-between align-items-center my-2">
+                    <div className="d-flex flex-column">
+                      <h3>{job.title}</h3>
+                      <h6>Company: {job.companyName}</h6>
+                      <h6>Deadline: {job.deadline}</h6>
+                    </div>
+                    <div>
+                      <Link
+                        to={`/jobs/${job.jobId}`}
+                        className="btn btn-secondary btn-lg mx-2"
+                      >
+                        View Job
+                      </Link>
+                    </div>
                   </div>
-                  <div>
-                    <Link
-                      to={`/jobs/${job.jobId}`}
-                      className="btn btn-secondary btn-lg mx-2"
-                    >
-                      View Job
-                    </Link>
-                  </div>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
