5bcf31558a06ca3707e1f68c1335fb522c5ebf51
Suven-p <pandeysuven@gmail.com>
2022-03-06 09:52:11 +0545

    Add procedure for searching jobs

--- config/sqldump.sql
+++ config/sqldump.sql
@@ -840,6 +840,36 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `searchJobs` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`webapp`@`localhost` PROCEDURE `searchJobs`(
+	IN query VARCHAR(5000)
+)
+BEGIN
+   SELECT JSON_ARRAYAGG(
+	 	JSON_OBJECT('jobId', j.jobId, 
+			'title', j.title,
+			'companyName', od.name,
+			'deadline', j.deadline
+		)
+	 ) as jobs
+	FROM jobs as j
+	INNER JOIN organization_data as od ON od.id = j.companyId
+	WHERE MATCH(j.title, j.description) AGAINST(query IN NATURAL LANGUAGE MODE);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `updateApplicant` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -911,4 +941,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-06  9:42:51
+-- Dump completed on 2022-03-06  9:52:13
