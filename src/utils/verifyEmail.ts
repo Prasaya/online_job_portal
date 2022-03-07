e8f822c24d41a5d51d34f808e3328d14f535ef79
Prasaya <prasaya.acharya@gmail.com>
2022-03-07 22:29:35 +0545

    Corrected details to send in verification email

--- src/utils/verifyEmail.ts
+++ src/utils/verifyEmail.ts
@@ -3,13 +3,13 @@ import sgMail from './sendMail';
 
 export default async function sendVerificationEmail(user: User, token: string) {
   const msg = {
-    to: user.email,
+    to: user.basics.email,
     from: 'accounts@nepaljobs.cf',
     templateId: 'd-b76987e2268f4a6bb31f352165eb316d',
     dynamic_template_data: {
       siteUrl: 'http://prasaya.eastus.cloudapp.azure.com/',
       iconUrl: 'https://prasaya.eastus.cloudapp.azure.com/logo512.png',
-      username: user.firstName || 'New User',
+      username: user.basics.firstName || 'New User',
       verifyUrl: `http://prasaya.eastus.cloudapp.azure.com/verify-email?token=${token}`,
     },
   };
