b7e8c77821d876c5c6fea5ab80f9722ce9e56223
Suven-p <pandeysuven@gmail.com>
2022-03-07 14:58:25 +0545

    .

--- python/app/profiler.py
+++ python/app/profiler.py
@@ -3,37 +3,19 @@ from SkillParser import SkillParser
 import json
 import yappi
 import sys
+from app import main as app_main
 # gprof2dot --format=callgrind --output=out.dot ./stats.callgrind; dot -Tpng out.dot -o graph.png
 
-async def main():
-    yappi.set_clock_type('WALL')
-    skill = SkillParser()
-    data1 = await skill.parse(['python', 'C++', 'Django', 'Nodejs'])
-    yappi.start()
-    data2 = await skill.parse([
-        "Coordinate and responsible for the company's HR work",
-        "Responsible for Nepalese's labour policy implementation and HR outreach work",
-        "Responsible for the formulation of Nepalese's local HR policy, and the promotion and implementation of important work tasks",
-        "Responsible for Nepalese's staff recruitment & allocation, compensation & benefits, and performance management",
-        "Responsible for Nepalese staffs' remuneration calculation and salary data statistics",
-        "Responsible for post & designation management, post & designation adjustment, post & designation transfer, etc.",
-        "Responsible for Nepalese's staff training, and cultural integration policy formulation",
-        "Responsible for HR team building and individual training",
-        "Responsible for department internal audit",
-        "Responsible for the preparation of company's manpower cost budget"
-    ])
-    # data2 = await skill.parse(['python', 'C++', 'writing', 'design', 'Django', 'Nodejs'])
-    # data3 = await skill.parse(['python', 'C++', 'writing', 'design', 'Django', 'Nodejs'])
-    # data4 = await skill.parse(['python', 'C++', 'writing', 'design', 'Django', 'Nodejs'])
-    # data5 = await skill.parse(['python', 'C++', 'writing', 'design', 'Django', 'Nodejs'])
-    yappi.stop()
-    # stats = yappi.get_func_stats(
-    #     filter_callback=lambda x: 'Esco' in x.module or 'DBPedia' in x.module)
-    stats = yappi.get_func_stats()
-    stats.save('stats.callgrind', type='callgrind')
-    stats.print_all()
-    await skill.close()
 
-
-if __name__ == '__main__':
-    asyncio.run(main())
+asyncio.run(app_main())
+yappi.set_clock_type('wall')
+yappi.clear_stats()
+yappi.start()
+asyncio.run(app_main())
+yappi.stop()
+stats = yappi.get_func_stats(
+    filter_callback=lambda x: 'Esco' in x.module or 'DBPedia' in x.module)
+stats = yappi.get_func_stats()
+stats.save('stats.callgrind', type='callgrind')
+with open('profiler.log', 'w') as f:
+    stats.print_all(out=f)
