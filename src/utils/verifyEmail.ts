f242265742fe8a66ce18d997acf094f02d11fd62
Prasaya <prasaya.acharya@gmail.com>
2022-03-10 01:27:35 +0545

    Change url

--- src/utils/verifyEmail.ts
+++ src/utils/verifyEmail.ts
@@ -7,10 +7,10 @@ export default async function sendVerificationEmail(user: User, token: string) {
     from: 'accounts@nepaljobs.cf',
     templateId: 'd-b76987e2268f4a6bb31f352165eb316d',
     dynamic_template_data: {
-      siteUrl: 'http://prasaya.eastus.cloudapp.azure.com/',
+      siteUrl: 'https://nepaljobs.cf/',
       iconUrl: 'https://prasaya.eastus.cloudapp.azure.com/logo512.png',
       username: user.basics.firstName || 'New User',
-      verifyUrl: `http://prasaya.eastus.cloudapp.azure.com/verify-email?token=${token}`,
+      verifyUrl: `https://nepaljobs.cf/verify-email?token=${token}`,
     },
   };
   await sgMail.send(msg);
