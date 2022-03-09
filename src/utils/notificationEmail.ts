931a8f17ab9fceb63089cd526d800903f44881e4
Prasaya <prasaya.acharya@gmail.com>
2022-03-09 14:17:42 +0545

    Refactor sending email notifications

--- src/utils/notificationEmail.ts
+++ src/utils/notificationEmail.ts
@@ -1,19 +1,27 @@
+import { getOrganizationById } from '@models/Organization';
 import JobPost from '@typings/JobPost';
+import DBJob from '@typings/Jobs';
 import { User } from '@typings/User';
+import Connection from 'mysql2/typings/mysql/lib/Connection';
 import sgMail from './sendMail';
 
 // TODO: change templateId and dynamic_template_data according to template
-export default async function sendNotificationEmail(user: User, job: JobPost) {
+export default async function sendNotificationEmail(user: User, job: DBJob) {
+    let companyDetails = (await getOrganizationById(job.companyId))[0];
+
     const msg = {
         to: user.basics.email,
         from: 'accounts@nepaljobs.cf',
-        templateId: 'd-b76987e2268f4a6bb31f352165eb316d',
+        templateId: 'd-e2b989853677487482ebcfb8fe24b075',
         dynamic_template_data: {
             siteUrl: 'http://prasaya.eastus.cloudapp.azure.com/',
             iconUrl: 'https://prasaya.eastus.cloudapp.azure.com/logo512.png',
             username: user.basics.firstName || 'New User',
-            verifyUrl: `http://prasaya.eastus.cloudapp.azure.com/verify-email?token=${token}`,
+            jobTitle: job.title,
+            companyName: companyDetails.name,
+            jobUrl: `http://prasaya.eastus.cloudapp.azure.com/job/${job.jobId}`,
         },
     };
+
     await sgMail.send(msg);
 }
