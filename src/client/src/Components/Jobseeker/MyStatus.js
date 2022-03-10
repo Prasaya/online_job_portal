d4ef4e52f808e85d26bc7610a314bc667ee2741e
Suprim Shrestha <suprim.shresth@gmail.com>
2022-03-10 13:38:51 +0545

    Display ranking

--- src/client/src/Components/Jobseeker/MyStatus.js
+++ src/client/src/Components/Jobseeker/MyStatus.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 function MyStatus() {
   const [jobs, setJobs] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
   async function fetchJobs() {
     const res = await fetch('/api/applicant/jobs');
@@ -13,10 +14,23 @@ function MyStatus() {
   useEffect(() => {
     async function getJobs() {
       const overviewJobs = await fetchJobs();
+      overviewJobs.jobs.map((job) => {
+        (async () => {
+          const res = await fetch(`/api/applicant/jobs/rank/${job.jobId}`);
+          const data = await res.json();
+          job.ranking = data.data;
+          return job;
+        })();
+      });
       setJobs(overviewJobs.jobs);
+      setTimeout(() => setLoading(false), 1000);
     }
     getJobs();
   }, []);
+
+  if (isLoading) {
+    return <>Loading...</>;
+  }
 
   return (
     <div className="mystatus card my-5">
@@ -31,15 +45,13 @@ function MyStatus() {
                     <h3>{job.title}</h3>
                     <h6>Company: {job.companyName}</h6>
                     <h6>Deadline: {job.deadline}</h6>
+                    <h6>
+                      Ranking: {job.ranking.rank} /{' '}
+                      {job.ranking.totalApplicants}
+                    </h6>
                   </div>
                   <div>
                     <div className="buttons">
-                      <Link
-                        to={`/jobs/${job.jobId}`}
-                        className="btn btn-primary btn-lg m-2"
-                      >
-                        View Ranking
-                      </Link>
                       <Link
                         to={`/jobs/${job.jobId}`}
                         className="btn btn-secondary btn-lg m-2"
--- src/routes/api/applicant/jobs.ts
+++ src/routes/api/applicant/jobs.ts
@@ -10,37 +10,33 @@ router.get('/rank/:id', async (req, res) => {
   try {
     const jobId = req.params.id;
     const applicantId = req.user?.user.basics.id;
-    const [result]: [RowDataPacket[], FieldPacket[]] =
-      await connection.execute(
-        `SELECT position from (
+    const [result]: [RowDataPacket[], FieldPacket[]] = await connection.execute(
+      `SELECT position from (
         SELECT aj.jobId, aj.applicantId,
                 RANK() OVER(ORDER BY jm.score DESC) position
         FROM jobMatchScore as jm
         INNER JOIN applicant_jobs as aj ON(jm.jobId = aj.jobId) AND(aj.applicantId = jm.applicantId)  
         ORDER BY position DESC) as val
             WHERE(val.jobId = ?) AND(val.applicantId = ?) `,
-        [jobId, applicantId],
-      );
-    const [count]: [RowDataPacket[], FieldPacket[]] =
-      await connection.execute(
-        `select count(*) as count from applicant_jobs
+      [jobId, applicantId],
+    );
+    const [count]: [RowDataPacket[], FieldPacket[]] = await connection.execute(
+      `select count(*) as count from applicant_jobs
           where jobId = ? `,
-        [jobId]
-      );
+      [jobId],
+    );
 
     let data = {
-      rank: result[0].position,
-      totalApplicants: count[0].count
-    }
+      rank: result.length > 0 ? result[0].position : 1,
+      totalApplicants: count[0].count,
+    };
 
     res.json({ data, success: true });
   } catch (err) {
     logger.error(`Error in getting job details: ${err}`);
     res.status(500).json({ message: 'Something went wrong!', success: false });
   }
-}
-);
-
+});
 
 router.get('/', async (req, res) => {
   try {
