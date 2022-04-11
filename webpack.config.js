61006710e78ecd9b3343e81a546af599f78d04cd
Suven-p <pandeysuven@gmail.com>
2022-04-11 10:45:00 +0545

    Add separate webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,11 @@ const path = require("path");
 
 const dirname = __dirname;
 
+console.log("dirname", dirname, path.resolve("."));
 const config = {
   // @ts-ignore
   mode: process.env.NODE_ENV || "development",
-  entry: "./bin/www",
+  entry: "./packages/api/bin/www",
   output: {
     path: path.join(dirname, "dist"),
     filename: "backend.cjs",
@@ -22,19 +23,18 @@ const config = {
     ],
   },
   resolve: {
-    roots: [dirname],
     extensions: [".tsx", ".ts", ".js"],
     alias: {
-      "@middleware": path.resolve(dirname, "middleware"),
-      "@models": path.resolve(dirname, "models"),
-      "@routes": path.resolve(dirname, "routes"),
-      "@utils": path.resolve(dirname, "utils"),
-      "@typings": path.resolve(dirname, "typings"),
-      "@root": dirname,
+      "@middleware": path.resolve(dirname, "packages/api/middleware"),
+      "@models": path.resolve(dirname, "packages/api/models"),
+      "@routes": path.resolve(dirname, "packages/api/routes"),
+      "@utils": path.resolve(dirname, "packages/api/utils"),
+      "@typings": path.resolve(dirname, "packages/api/typings"),
+      "@root": path.resolve(dirname, "packages/api"),
     },
   },
   externalsPresets: { node: true },
-  // externals: [nodeExternals()],
+  externals: [nodeExternals()],
 };
 
 module.exports = config;
