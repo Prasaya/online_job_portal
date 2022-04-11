996a5e3eeed7319b3c7ea6dbb14e5a12ba22a91b
Suven-p <pandeysuven@gmail.com>
2022-04-11 16:08:21 +0545

    Add workspace and config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,48 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const nodeExternals = require("webpack-node-externals");
-const path = require("path");
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
 
 const dirname = __dirname;
 
-console.log("dirname", dirname, path.resolve("."));
 const config = {
   // @ts-ignore
-  mode: process.env.NODE_ENV || "development",
-  entry: "./packages/api/bin/www",
+  mode: process.env.NODE_ENV || 'development',
+  entry: './packages/api/bin/www',
   output: {
-    path: path.join(dirname, "dist"),
-    filename: "backend.cjs",
+    path: path.join(dirname, 'dist'),
+    filename: 'backend.cjs',
+  },
+  watchOptions: {
+    ignored: [
+      '.vscode',
+      '.yarn',
+      'dist',
+      'Local_API_v0_19_4',
+      'logs',
+      '**/node_modules',
+      'python',
+      'scripts',
+      'test',
+    ],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
         exclude: [/node_modules/],
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
     alias: {
-      "@middleware": path.resolve(dirname, "packages/api/middleware"),
-      "@models": path.resolve(dirname, "packages/api/models"),
-      "@routes": path.resolve(dirname, "packages/api/routes"),
-      "@utils": path.resolve(dirname, "packages/api/utils"),
-      "@typings": path.resolve(dirname, "packages/api/typings"),
-      "@root": path.resolve(dirname, "packages/api"),
+      '@middleware': path.resolve(dirname, 'packages/api/middleware'),
+      '@models': path.resolve(dirname, 'packages/api/models'),
+      '@routes': path.resolve(dirname, 'packages/api/routes'),
+      '@utils': path.resolve(dirname, 'packages/api/utils'),
+      '@typings': path.resolve(dirname, 'packages/api/typings'),
+      '@root': path.resolve(dirname, 'packages/api'),
     },
   },
   externalsPresets: { node: true },
