7766ea11827d0968fd82495111177d33ec02d24f
Suven-p <pandeysuven@gmail.com>
2022-04-15 17:09:27 +0545

    Switch to typescript

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.css';
 import { UserContextProvider } from './Context/UserContext';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <UserContextProvider>
       <App />
     </UserContextProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 );
